51bf8614ea35ec96dd6d4f9addaff65bdc0fe1de
Amanda Santana <asantana@thoughtworks.com>
2020-05-18 19:16:52 -0300

    Add main methods and smoke tests

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const search = () => {};
+const searchAlbums = () => {};
+const searchArtists = () => {};
+const searchTracks = () => {};
+const searchPlaylists = () => {};
+
+export {
+  search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
+};
