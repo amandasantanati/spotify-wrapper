560adb95129cb329d3a2774e2234b7b2a68aeb12
Amanda Santana <asantana@thoughtworks.com>
2020-05-19 18:06:26 -0300

    Quick fix lint error and tests

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-const search = (query, type) => {
-  return fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
-    .then((data) => data.json());
-};
+const search = (query, type) => global.fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+  .then((data) => data.json());
 
 const searchAlbums = () => {};
 const searchArtists = () => {};
