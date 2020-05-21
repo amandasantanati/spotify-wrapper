77c3d17f113e980d6060546ae2384c35496bf844
Amanda Santana <amandasantanati@gmail.com>
2020-05-21 09:48:36 -0300

    Refactor URL to const.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /* global fetch */
+import API_URL from './config';
 
-const search = (query, type) => fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`)
   .then((data) => data.json());
 
 const searchAlbums = (query) => search(query, 'album');
